684d9446117bd06182047a003ea8215efd758d2a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-07 20:27:12 -0200

    Add default permissions
    Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

--- config/weather.mk
+++ config/weather.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
     vendor/lineage/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/lineage/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
 
