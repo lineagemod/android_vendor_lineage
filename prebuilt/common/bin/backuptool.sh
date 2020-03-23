ViperOS/android_vendor_viper

backuptool: fix for 9
 * Test: flash rom + gapps, flash only rom = gapps still there

Change-Id: If2946e1c61bbffca269ec8f255186609f7e277dd

prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=16.0
+export V=9
 
 export ADDOND_VERSION=1
 
@@ -33,6 +33,19 @@ preserve_addon_d() {
   fi
 }
 
+# Proceed only if /system is the expected ViperOS version
+check_prereq() {
+# If there is no build.prop file the partition is probably empty.
+if [ ! -r /system/build.prop ]; then
+    return 0
+fi
+#if ( ! grep -q "^ro.viper.version=$V.*" /system/build.prop ); then
+#  echo "Not backing up files from incompatible version: $V"
+#  return 0
+#fi
+return 1
+}
+
 # Restore /system/addon.d from /tmp/addon.d
 restore_addon_d() {
   if [ -d /tmp/addon.d/ ]; then
@@ -40,19 +53,6 @@ restore_addon_d() {
     cp -a /tmp/addon.d/* /system/addon.d/
     rm -rf /tmp/addon.d/
   fi
-}
-
-# Proceed only if /system is the expected major and minor version
-check_prereq() {
-# If there is no build.prop file the partition is probably empty.
-if [ ! -r /system/build.prop ]; then
-    return 0
-fi
-if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
-  echo "Not backing up files from incompatible version: $V"
-  return 0
-fi
-return 1
 }
 
 check_blacklist() {

lineagemod/android_vendor_lineage

prebuilt/common/bin/backuptool.sh
@@ -4,8 +4,8 @@
 #
 
 export C=/tmp/backupdir
-export S=$2
-export V=16.0
+export S=/system
+export V=9
 
 export ADDOND_VERSION=1
 
@@ -33,6 +33,19 @@ preserve_addon_d() {
   fi
 }
 
+# Proceed only if /system is the expected major and minor version
+check_prereq() {
+# If there is no build.prop file the partition is probably empty.
+if [ ! -r /system/build.prop ]; then
+    return 0
+fi
+#if ( ! grep -q "^ro.lineage.version=$V.*" /system/build.prop ); then
+#  echo "Not backing up files from incompatible version: $V"
+#  return 0
+#fi
+return 1
+}
+
 # Restore /system/addon.d from /tmp/addon.d
 restore_addon_d() {
   if [ -d /tmp/addon.d/ ]; then
@@ -40,19 +53,6 @@ restore_addon_d() {
     cp -a /tmp/addon.d/* $S/addon.d/
     rm -rf /tmp/addon.d/
   fi
-}
-
-# Proceed only if /system is the expected major and minor version
-check_prereq() {
-# If there is no build.prop file the partition is probably empty.
-if [ ! -r $S/build.prop ]; then
-    return 0
-fi
-if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
-  echo "Not backing up files from incompatible version: $V"
-  return 0
-fi
-return 1
 }
 
 check_blacklist() {